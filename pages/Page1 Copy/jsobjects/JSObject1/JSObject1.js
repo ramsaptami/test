5e17f5764ef1aa843eb303545777b2df2b19add2
john doe <wedytfuhjkjlhtdefhgcjvkbmn@gmail.com>
2023-08-31 15:36:53 +0000

    System generated commit, initial commit

---
+++ pages/Page1 Copy/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		return math_random.prototype
+	},
+	async myFun2 () {
+		ansi_regex.call(() => {
+			showAlert('success')
+		})
+		
+		
+	}
+}
